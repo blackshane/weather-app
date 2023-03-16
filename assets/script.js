57ed4bc6c2828f2337dcc6814fcae4fb0167cd40
blackshane <122633142+blackshane@users.noreply.github.com>
2023-03-16 16:12:32 -0500

    styling improvements. aside is actually to side.

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,4 @@
 var APIkey = '4db82ccb8ba6333d32b8c06c5e939250'
 var city;
+var weatherURL = 'api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}'
+var coordByLocNameURL ='http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}'
