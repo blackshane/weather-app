8c3587931d5dc451a883e08b36c84ebc7c0c9883
blackshane <122633142+blackshane@users.noreply.github.com>
2023-03-19 12:54:45 -0500

    5 day forecast works. some styling updates- screen not 100% responsive

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,5 @@
 var APIkey = '4db82ccb8ba6333d32b8c06c5e939250'
 var city = document.getElementById('city-search')
-
 var weatherURL = 'http://api.openweathermap.org/data/2.5/forecast?lat=38.81&lon=-90.69&units=imperial&appid=' + APIkey 
 var coordByLocNameURL ='http://api.openweathermap.org/geo/1.0/direct?q=O\'Fallon,MO,&limit=5&appid=' + APIkey
 var searchBarCity = document.getElementById('search');
@@ -23,17 +22,28 @@ function convertToLatAndLon() {
         function runUpdatedLocation(){
           
                 console.log('get API running')
-                fetch(weatherURL) //need to be able to search not just my weather
+                fetch(weatherURL)
                 .then(function(response) {
                     return response.json();
                 })
                 .then(function (data) {
                 console.log('Fetch Response \n-------------');
                 console.log(data);
-                const temp = data.list[0].main.temp;
+                const temp = data.list[0].main.temp.toFixed(1)+ '°';
                 const currentCity = data.city.name;
+                const tomorrowForecast = data.list[6].main.temp.toFixed(1) + '°';
+                const twoDayForecast = data.list[14].main.temp.toFixed(1) + '°';
+                const threeDayForecast = data.list[22].main.temp.toFixed(1) + '°';
+                const fourDayForecast = data.list[30].main.temp.toFixed(1) + '°';
+                const fiveDayForecast = data.list[38].main.temp.toFixed(1) + '°';
                 document.getElementById('current-temp').textContent = temp;
                 document.getElementById('location').textContent = currentCity;
+                document.getElementById('next-day-forecast').textContent = tomorrowForecast;
+                document.getElementById('two-day-forecast').textContent = twoDayForecast
+                document.getElementById('three-day-forecast').textContent = threeDayForecast;
+                document.getElementById('four-day-forecast').textContent = fourDayForecast;
+                document.getElementById('five-day-forecast').textContent = fiveDayForecast;
+                
                 console.log(temp)
             });
         
@@ -54,7 +64,6 @@ convertToLatAndLon();
 
 
 // update "weatherURL" to work for city in search bar. 
-// save past searches to local storage. 
-// display data in correct fields. 
+// save past searches to local storage.  
 
-// need to take var = {} and put that variable in the url search for lat and log
+//maybe use AppendChild for the list of previously saved cities? and create the list dynamically if local storage somehow can't achieve this. 
