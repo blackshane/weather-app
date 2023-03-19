4e3e51a17e14fe9f0302be705c27ef4d68eda1eb
blackshane <122633142+blackshane@users.noreply.github.com>
2023-03-18 22:16:42 -0500

    function to convert city to lat and lon

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,58 @@
 var APIkey = '4db82ccb8ba6333d32b8c06c5e939250'
-var city;
+var city = document.getElementById('city-search')
 
-var weatherURL = 'http://api.openweathermap.org/data/2.5/forecast?lat=38.81&lon=-90.69&units=imperial&appid=4db82ccb8ba6333d32b8c06c5e939250'
-var coordByLocNameURL ='http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}'
-var searchBar = document.getElementById('search');
+var weatherURL = 'http://api.openweathermap.org/data/2.5/forecast?lat=38.81&lon=-90.69&units=imperial&appid=' + APIkey 
+var coordByLocNameURL ='http://api.openweathermap.org/geo/1.0/direct?q=O\'Fallon,MO,&limit=5&appid=' + APIkey
+var searchBarCity = document.getElementById('search');
 var searchButton = document.getElementById('search-btn');
 
-document.addEventListener('click',getAPI())
 
-function getAPI() {
 
-    console.log('get API running')
-    fetch(weatherURL) //need to be able to search not just my weather
-    .then(function(response) {
+function convertToLatAndLon() {
+    fetch(coordByLocNameURL)
+    .then(function(response){
         return response.json();
     })
-    .then(function (data) {
-    console.log('Fetch Response \n-------------');
-    console.log(data);
-    const temp = data.list[0].main.temp;
-    const currentCity = data.city.name;
-    document.getElementById('current-temp').textContent = temp;
-    document.getElementById('location').textContent = currentCity;
-    console.log(temp)
-  });
+    .then(function(data) {
+        console.log(data)
+        const cityLat = data[0].lat;
+        console.log(cityLat);
+        const cityLon = data[0].lon;
+        console.log(cityLon);
+        let weatherURL = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + cityLat + '&lon=' + cityLon +'&units=imperial&appid=' + APIkey;
+        function runUpdatedLocation(){
+          
+                console.log('get API running')
+                fetch(weatherURL) //need to be able to search not just my weather
+                .then(function(response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                console.log('Fetch Response \n-------------');
+                console.log(data);
+                const temp = data.list[0].main.temp;
+                const currentCity = data.city.name;
+                document.getElementById('current-temp').textContent = temp;
+                document.getElementById('location').textContent = currentCity;
+                console.log(temp)
+            });
+        
+                
+      }
+      runUpdatedLocation()
+    
 }
-getAPI()
+)
+}  
+    
+convertToLatAndLon();
 
-// add listener event to submit button on the search bar to run getAPI function.
+
+// searchButton.addEventListener('click', getAPI)
+
+
+
+
 // update "weatherURL" to work for city in search bar. 
 // save past searches to local storage. 
 // display data in correct fields. 
