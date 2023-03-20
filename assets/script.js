37235b08a351f07aecab62b7dbf34a69e0ed2547
blackshane <122633142+blackshane@users.noreply.github.com>
2023-03-20 09:54:31 -0500

    future temp and forecast display correctly

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,18 @@
 var APIkey = '4db82ccb8ba6333d32b8c06c5e939250'
 var city = document.getElementById('city-search')
+var searchButton = document.getElementById('search-btn');
 var weatherURL = 'http://api.openweathermap.org/data/2.5/forecast?lat=38.81&lon=-90.69&units=imperial&appid=' + APIkey 
 var coordByLocNameURL ='http://api.openweathermap.org/geo/1.0/direct?q=O\'Fallon,MO,&limit=5&appid=' + APIkey
 var searchBarCity = document.getElementById('search');
-var searchButton = document.getElementById('search-btn');
 
 
+searchButton.addEventListener('click', getDataFromSearch()) 
+
+function getDataFromSearch() {
+   
+    var text = city.value;
+    console.log('this is' + text);    
+}
 
 function convertToLatAndLon() {
     fetch(coordByLocNameURL)
@@ -31,20 +38,32 @@ function convertToLatAndLon() {
                 console.log(data);
                 const temp = data.list[0].main.temp.toFixed(1)+ '°';
                 const currentCity = data.city.name;
-                const tomorrowForecast = data.list[6].main.temp.toFixed(1) + '°';
-                const twoDayForecast = data.list[14].main.temp.toFixed(1) + '°';
-                const threeDayForecast = data.list[22].main.temp.toFixed(1) + '°';
-                const fourDayForecast = data.list[30].main.temp.toFixed(1) + '°';
-                const fiveDayForecast = data.list[38].main.temp.toFixed(1) + '°';
+                const tomorrowTemp = data.list[6].main.temp.toFixed(1) + '°';
+                const twoDayTemp = data.list[14].main.temp.toFixed(1) + '°';
+                const threeDayTemp = data.list[22].main.temp.toFixed(1) + '°';
+                const fourDayTemp = data.list[30].main.temp.toFixed(1) + '°';
+                const fiveDayTemp = data.list[38].main.temp.toFixed(1) + '°';
+                const currentForecast = data.list[0].weather[0].main;
+                const tomorrowForecast = data.list[6].weather[0].main;
+                const twoDayForecast = data.list[14].weather[0].main;
+                const threeDayForecast = data.list[22].weather[0].main;
+                const fourDayForecast = data.list[30].weather[0].main;
+                const fiveDayForecast = data.list[38].weather[0].main;
                 document.getElementById('current-temp').textContent = temp;
                 document.getElementById('location').textContent = currentCity;
-                document.getElementById('next-day-forecast').textContent = tomorrowForecast;
-                document.getElementById('two-day-forecast').textContent = twoDayForecast
-                document.getElementById('three-day-forecast').textContent = threeDayForecast;
-                document.getElementById('four-day-forecast').textContent = fourDayForecast;
-                document.getElementById('five-day-forecast').textContent = fiveDayForecast;
+                document.getElementById('next-day-forecast').textContent = tomorrowTemp;
+                document.getElementById('two-day-forecast').textContent = twoDayTemp
+                document.getElementById('three-day-forecast').textContent = threeDayTemp;
+                document.getElementById('four-day-forecast').textContent = fourDayTemp;
+                document.getElementById('five-day-forecast').textContent = fiveDayTemp;
+                document.getElementById('current-sun').textContent = currentForecast;
+                document.getElementById('tomorrow-sun').textContent = tomorrowForecast;
+                document.getElementById('two-day-sun').textContent = tomorrowForecast;
+                document.getElementById('three-day-sun').textContent = tomorrowForecast;
+                document.getElementById('four-day-sun').textContent = tomorrowForecast;
+                document.getElementById('five-day-sun').textContent = tomorrowForecast;
                 
-                console.log(temp)
+                console.log(currentForecast, tomorrowForecast, twoDayForecast, threeDayForecast, fourDayForecast, fiveDayForecast)
             });
         
                 
